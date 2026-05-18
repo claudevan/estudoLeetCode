6948822b76ff823445f5e52b27394ada07b88622
Claudevan <claudevan.ti@gmail.com>
2026-05-18 00:39:17 -0300

    test: add Exercise238Tests

---
+++ estudoLeetCode/Tests/Exercise238Tests.cs
@@ -0,0 +1,17 @@
+﻿using Xunit;
+using estudoLeetCode;
+
+namespace Tests;
+
+public class Exercise238Tests
+{
+    [Theory]
+    [InlineData(new[] { 1, 2, 3, 4 }, new[] { 24, 12, 8, 6 })]
+    [InlineData(new[] { -1, 1, 0, -3, 3 }, new[] { 0, 0, 9, 0, 0 })]
+    public void Result(int[] nums, int[] expected)
+    {
+        var exercise = new Exercise238();
+        var result = exercise.ProductExceptSelf(nums);
+        Assert.Equal(expected, result);
+    }
+}
